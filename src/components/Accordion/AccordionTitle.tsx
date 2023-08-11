7872fc5a119b0ca19e940f3f8bd0c619ebc3439b
Alexander Kachan <akach.dev@mail.ru>
2023-08-11 19:46:31 +0300

    initial commit

--- src/components/Accordion/AccordionTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type  AccordionTitlePropsType = {
-  title: string
-}
-
-export function AccordionTitle({title}: AccordionTitlePropsType) {
-  return <h3>{title}</h3>;
-}
