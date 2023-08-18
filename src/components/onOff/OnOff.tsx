28ec31115bfc4237a574172ba41c85b0537c9364
Alexander Kachan <akach.dev@mail.ru>
2023-08-18 18:04:03 +0300

    add component OnOff

---
+++ src/components/onOff/OnOff.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const OnOff = () => {
+  return (
+     <div>
+       <span>On</span>
+       <span>Off</span>
+       <span className={'bulb'}></span>
+     </div>
+  );
+};
