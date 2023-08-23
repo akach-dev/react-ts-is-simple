85a3fd1d68167c238b5c054e7c1a465d22792dc0
Alexander Kachan <akach.dev@mail.ru>
2023-08-23 18:11:34 +0300

    add onHandler, offHandler

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export function App() {
        <Rating value={3}/>
        <Rating value={4}/>
        <Rating value={5}/>
-       <OnOff bulb={true}/>
+       <OnOff/>
      </>
   )
 }
--- src/components/onOff/OnOff.tsx
+++ src/components/onOff/OnOff.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 type OnOffPropsType = {
-  bulb: boolean
+  // bulb: boolean
 }
 
-export const OnOff = ({bulb}: OnOffPropsType) => {
+export const OnOff = (props: OnOffPropsType) => {
+
+  let bulb
+
+  const onHandler = () => bulb = true
+  const offHandler = () => bulb = true
+
+
   return (
      <div className={'onOff'}>
-       <span className={bulb ? 'on' : ''}>On</span>
-       <span className={!bulb ? 'off' : ''}>Off</span>
+       <span
+          onClick={}
+          className={bulb ? 'on' : ''}>On</span>
+       <span
+          onClick={}
+          className={!bulb ? 'off' : ''}>Off</span>
        <span className={bulb ? 'on bulb' : 'off bulb'}> </span>
      </div>
   );
