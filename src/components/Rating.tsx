f4a7c97517396014cf77daae66807591166cd47f
Alexander Kachan <akach.dev@mail.ru>
2023-08-29 21:56:10 +0300

    up rating

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,26 +1,17 @@
 import {Star} from "./Star";
 import React, {FC, useState} from "react";
 
-type RatingPropsType = {
-  // value: 0 | 1 | 2 | 3 | 4 | 5
-}
 
-export const Rating: FC<RatingPropsType> = () => {
+export const Rating: FC = () => {
   const [select, setSelect] = useState(0)
 
-  const selectHandler = (value: number) => setSelect(value)
   return (
      <div>
-       <Star selected={select > 0}/>
-       <button onClick={() => selectHandler(1)}>1</button>
-       <Star selected={select > 1}/>
-       <button onClick={() => selectHandler(2)}>2</button>
-       <Star selected={select > 2}/>
-       <button onClick={() => selectHandler(3)}>3</button>
-       <Star selected={select > 3}/>
-       <button onClick={() => selectHandler(4)}>4</button>
-       <Star selected={select > 4}/>
-       <button onClick={() => selectHandler(5)}>5</button>
+       <Star value={1} setSelect={setSelect} selected={select > 0}/>
+       <Star value={2} setSelect={setSelect} selected={select > 1}/>
+       <Star value={3} setSelect={setSelect} selected={select > 2}/>
+       <Star value={4} setSelect={setSelect} selected={select > 3}/>
+       <Star value={5} setSelect={setSelect} selected={select > 4}/>
      </div>
   )
 }
