3acc26b3f6bd52f1025e5981a914c45224ec90e2
Alexander Kachan <akach.dev@mail.ru>
2023-09-23 10:05:16 +0300

    lesson useCallback done

--- src/stories/React.memo.stories.tsx
+++ src/stories/React.memo.stories.tsx
@@ -1,4 +1,4 @@
-import {memo, useMemo, useState} from "react";
+import {FC, memo, useCallback, useMemo, useState} from "react";
 
 export default {
   title: 'React.memo'
@@ -17,7 +17,7 @@ const Users = memo(({users}: UsersPropsType) => {
   </div>
 })
 
-export const HelpsToReactMemo = () => {
+export const HelpsToReactMemoWithUseMemo = () => {
   console.log('HelpsToReactMemo')
   const [counter, setCounter] = useState(0)
   const [users, setUsers] = useState(['Alex', 'Oksana', 'Max'])
@@ -28,15 +28,54 @@ export const HelpsToReactMemo = () => {
     setUsers([...users, 'Misha' + new Date().getTime()])
   }
 
-  const filterUsers = useMemo(() => users.filter(user => user.toLowerCase().includes('m')), [users])
+  // const filterUsers = useMemo(() => users.filter(user => user.toLowerCase().includes('m')), [users])
+  const filterUsers = users.filter(user => user.toLowerCase().includes('m'))
 
 
   return <div>
     <button onClick={increment}>+ {counter}</button>
     <button onClick={addUser}>Add User</button>
 
-    <Users users={filterUsers}/>
+    <Users users={useMemo(() => filterUsers, [users])}/>
   </div>
 
 }
+export const LikeUseCallback: FC = () => {
+  console.log('LikeUseCallback')
+  const [counter, setCounter] = useState(0)
+  const [books, setBooks] = useState(['React ', 'JS'])
+
+  const increment = () => setCounter(counter + 1)
+
+  const addBook = () => {
+    console.log(books)
+    setBooks([...books, 'StoryBook' + new Date().getTime()])
+  }
+
+  // const filterUsers = useMemo(() => users.filter(user => user.toLowerCase().includes('m')), [users])
+  const filterBooks = books.filter(book => book.toLowerCase().includes('r'))
+
+  return <div>
+    <button onClick={increment}>+ {counter}</button>
+    <Books
+       addBook={useCallback(addBook, [books])}
+       books={useMemo(() => filterBooks, [books])}/>
+  </div>
+}
+
+type BooksPropsType = {
+  books: string[]
+  addBook: () => void
+}
+
+const Books: FC<BooksPropsType> = memo(({books, addBook}) => {
+  console.log('Books')
+  return <div>
+    <button onClick={addBook}>Add book</button>
+
+    {
+      books.map((book, index) => <div key={index}> {book}  </div>)
+    }
+  </div>
+})
 
