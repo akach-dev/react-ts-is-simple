b3b0f1d5fedbfd9899937b771ff1e6f3ae8996b1
Alexander Kachan <akach.dev@mail.ru>
2023-09-28 20:30:38 +0300

    lesson useEffect watch  done

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,11 +1,12 @@
 import {useCallback, useEffect, useMemo, useState} from "react";
+import {log} from "util";
 
 export default {
   title: 'useEffect demo'
 }
 
 
-export const Example1 = () => {
+export const SimpleExample1 = () => {
   const [counter, setCounter] = useState(0)
   const [fake, setFake] = useState(1)
   console.log('Example1')
@@ -18,6 +19,37 @@ export const Example1 = () => {
 
   return <div>
     <button onClick={() => setFake(fake + 1)}>+ {counter} {fake} </button>
+  </div>
+
+}
+export const SetTimeoutExample1 = () => {
+  const date = new Date()
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+
+  const [seconds, setSeconds] = useState(date.getSeconds())
+  console.log(seconds)
+
+  useEffect(() => {
+
+
+    const interval = setInterval(() => {
+      console.log('setInterval')
+      const updateSeconds = new Date().getSeconds()
+      setSeconds(updateSeconds)
+      document.title = `${hour}:${minutes}:${seconds + 1}`
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+
+
+  }, [hour, minutes, seconds]);
+
+
+  return <div>
+    {`${hour}:${minutes}:${seconds}`}
   </div>
 
 }
